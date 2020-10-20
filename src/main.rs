cb45dd6dd84a98e35dc4b69c7cd6c49b3d0e5a38
coltonhurst <colton@coltonhurst.com>
2020-10-20 11:59:07 -0400

    some refactoring + getting 1st arg using clone

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,29 @@
-use std::io::{self, Write};
+// https://play.rust-lang.org/?version=stable&mode=debug&edition=2018&gist=b7ce05386e388cf30a5cd8b8d33297e0
+
 use std::env;
+use std::io::{self, Write};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let mut zipcode = String::new();
 
-    if args.len() >= 2 {
-        //zipcode = args[1];
+    let zipcode = if args.len() >= 2 {
+        args[1].clone()
     } else {
-        print!("Please enter a zip code to use: ");
-        io::stdout().flush().unwrap();
-
-        match io::stdin().read_line(&mut zipcode) {
-            Ok(_) => {}
-            Err(error) => println!("error: {}", error),
-        }
-    }
+        get_zipcode_from_user()
+    };
 
     println!("The zipcode is {}", zipcode);
+}
+
+fn get_zipcode_from_user() -> String {
+    let mut zipcode = String::new();
+    print!("Please enter a zip code to use: ");
+    io::stdout().flush().unwrap();
+
+    match io::stdin().read_line(&mut zipcode) {
+        Err(error) => println!("error: {}", error),
+        _ => {}
+    }
+
+    zipcode
 }
